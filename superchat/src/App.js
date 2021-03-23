a91c7d5e277f837a4e2e81716b48e49f9a8ad554
aeeux <AndreasEriksen99@hotmail.com>
2021-03-23 13:55:23 +0100

    update
    update

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA3CfN1W2acBp1XCfOQbb6r7x84z2od-Do",
+  authDomain: "marketplace-superchat.firebaseapp.com",
+  projectId: "marketplace-superchat",
+  storageBucket: "marketplace-superchat.appspot.com",
+  messagingSenderId: "659109377555",
+  appId: "1:659109377555:web:cd4417bd47e77dadf5b1e6",
+  measurementId: "G-H29WSQDL97"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn /> }
+      </section>
+
     </div>
   );
+
 }
 
 export default App;
