2e1769e4e450bdbd5390ba88bfc9adc71d3a2cbd
aeeux <AndreasEriksen99@hotmail.com>
2021-03-23 14:54:20 +0100

    update
    update

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -20,6 +21,8 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
 
 function App() {
 
@@ -27,12 +30,13 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header>
+        <h1>🔥</h1>
+        <SignOut />
       </header>
 
       <section>
-        {user ? <ChatRoom /> : <SignIn />} //if user is signed in then show ChatRoom, if not then show SignIn
+        {user ? <ChatRoom /> : <SignIn />}
       </section>
 
     </div>
@@ -40,47 +44,86 @@ function App() {
 }
 
 function SignIn() {
+
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithGoogle(provider);
+    auth.signInWithPopup(provider);
   }
 
   return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Chat</button>
+      <p>Chat</p>
+    </>
   )
+
 }
 
 function SignOut() {
   return auth.currentUser && (
-
-    <button onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
+
 
 function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
 
-  const messageRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdArt').limit(25);
+  const [messages] = useCollectionData(query, { idField: 'id' });
 
-  const [messages] = useCollectionData(query, {idField: 'id'}); //listen to data with a hook, React will observe the changes in realtime.
+  const [formValue, setFormValue] = useState('');
 
-  return (
-    <>
-      <div>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-      </div>
 
-      <div>
+  const sendMessage = async (e) => {
+    e.preventDefault();
 
-      </div>
-    </>
-  )
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
 }
+
 
 function ChatMessage(props) {
-  const { text, uid } = props.message;
+  const { text, uid, photoURL } = props.message;
 
-  return <p>{text}</p>
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
 }
+
 
 export default App;
