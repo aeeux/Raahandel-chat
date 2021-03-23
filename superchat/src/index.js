0277045721d9c62ff90e5985cbe17f585ba6795e
aeeux <AndreasEriksen99@hotmail.com>
2021-03-23 15:59:24 +0100

    update
    update

--- superchat/src/index.js
+++ superchat/src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*  const functions = require('firebase-functions');
+  const Filter = require('bad-words');
+
+  const admin = require('firebase-admin');
+  admin.initializeApp();
+
+  const db = admin.firestore();
+
+  export.detectEvilUsers = functions.firestore
+        .document('messages/{msgId}')
+        .onCreate(async (doc, ctx) => {
+        
+        const filter = new Filter();
+        const { text, uid } = doc.data();
+
+        if (filter.isProfane(text)) {
+
+          const cleaned = filter.clean(text);
+          await doc.ref.update({text: '🤫 I got banned for saying.. ${cleaned}'});
+
+          await db.collection('banned').doc(uid).set({});
+
+        }
+
+
+
+      });
+*/
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
